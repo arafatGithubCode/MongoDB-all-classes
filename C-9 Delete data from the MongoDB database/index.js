1b03de55213beb694de6dacb891b5b10d524b695
arafatGithubCode <md489288@gmail.com>
2023-11-29 09:40:47 +0600

    Delete data from the MongoDB database

---
+++ C-9 Delete data from the MongoDB database/index.js
@@ -0,0 +1,136 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//connectDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/productDB");
+    console.log("db is connected");
+  } catch (error) {
+    console.log("db is not connected");
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
+//create productSchema
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+//create product model
+const Product = mongoose.model("products", productSchema);
+
+//create product
+app.post("/products", async (req, res) => {
+  try {
+    const inputProducts = req.body;
+    const productData = await Product.insertMany(inputProducts);
+    res.status(200).send({
+      success: true,
+      message: "products are created",
+      data: productData,
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+//Read products
+app.get("/products", async (req, res) => {
+  try {
+    const price = req.query.price;
+    const rating = req.query.rating;
+    let products;
+    if (price && rating) {
+      // products = await Product.find({
+      //   $or: [{ price: { $gt: price } }, { rating: { $gt: rating } }],
+      // }).countDocuments();
+
+      //1(accending) -1(decending)
+      products = await Product.find({
+        $or: [{ price: { $gt: price } }, { rating: { $gt: rating } }],
+      })
+        .sort({ price: -1 })
+        .select({ title: 1 });
+    } else {
+      // products = await Product.find().countDocuments();
+      products = await Product.find().sort({ price: -1 }).select({ title: 1 });
+    }
+    res.status(201).send({
+      success: true,
+      message: "products are created",
+      data: products,
+    });
+  } catch (error) {
+    res.status(401).send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+//delete operation
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    // const deleteProduct = await Product.deleteOne({ _id: id });
+    const deleteProduct = await Product.findByIdAndDelete({ _id: id });
+    if (deleteProduct) {
+      res.status(202).send({
+        success: true,
+        message: `A product was deleted with this id: ${id}}`,
+        data: deleteProduct,
+      });
+    } else {
+      res.status(402).send({
+        success: false,
+        message: `Products could not find with this id: ${id}`,
+      });
+    }
+  } catch (error) {
+    res.status(401).send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+//home route
+app.get("/", (req, res) => {
+  res.send("I'm home");
+});
+
+app.listen(PORT, async () => {
+  console.log(`server is running at http:localhost:${PORT}`);
+  await connectDB();
+});
